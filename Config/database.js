cf00fea6c77250e9e1a2ae7d4d7ce3782a8bb4e1
Sean Rooney <sean.rooney67@gmail.com>
2019-05-16 10:40:13 +0100

    mongoose update

--- Config/database.js
+++ Config/database.js
@@ -2,6 +2,6 @@ module.exports = {
   // configure the code below with your username, password and mlab database information
   //database: 'mongodb://127.0.0.1:27017',   //prod
   //database: 'mongodb://localhost:27017/meanauth',    //dev
-  database:'mongodb://SeanRoo:Celtic88@classroom-isery.mongodb.net/test?retryWrites=true',
+  database:'mongodb+srv://SeanRoo:Celtic88@classroom-isery.mongodb.net/test?retryWrites=true',
   secret: 'yoursecret'
 }
