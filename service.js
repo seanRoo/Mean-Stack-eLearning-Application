bb33e582970aeba8906f0969f8076dd4493824e5
Sean Rooney <sean.rooney67@gmail.com>
2019-05-17 22:41:22 +0100

    blah

--- service.js
+++ service.js
@@ -36,16 +36,11 @@ App.use(Parser.json());
 App.use(Parser.urlencoded( {extended: true} ));
 App.use(RequestLogger('dev')); // Replace with some good logging library eventually
 
-//App.use(Express.static(path.join(__dirname , 'public/')));
+App.use(Express.static(path.join(__dirname , 'public')));
 //App.use("/api", routes);
-App.get('*', function (req, res) {
-    res.sendfile('./public/index.html'); // load our index.html file
+App.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
   });
-
-// App.get('/', function(req, res) {
-//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
-// });
-
 // Passport Middleware
 App.use(passport.initialize());
 App.use(passport.session());
