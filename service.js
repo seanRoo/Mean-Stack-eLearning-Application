eafbea2b8412aeff991465df9557476f9cc49c03
Sean Rooney <sean.rooney67@gmail.com>
2019-05-17 19:23:34 +0100

    new build

--- service.js
+++ service.js
@@ -36,7 +36,7 @@ App.use(Parser.json());
 App.use(Parser.urlencoded( {extended: true} ));
 App.use(RequestLogger('dev')); // Replace with some good logging library eventually
 
-App.use(Express.static(path.join(__dirname , 'public')));
+App.use(Express.static(path.join(__dirname , 'public/')));
 //App.use("/api", routes);
 App.get('', function(req, res) {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
