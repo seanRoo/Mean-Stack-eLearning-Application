d39d3e5a57b9e8ae3fc67fe0bd7f65cc023724d7
Sean Rooney <sean.rooney67@gmail.com>
2019-05-17 23:51:13 +0100

    bluh

--- service.js
+++ service.js
@@ -25,7 +25,7 @@ const Generator = require('express-oas-generator');
 
 Mongoose.set('debug', true);
 const uri = 'mongodb://SeanRoo:Celtic88@classroom-shard-00-00-isery.mongodb.net:27017,classroom-shard-00-01-isery.mongodb.net:27017,classroom-shard-00-02-isery.mongodb.net:27017/test?ssl=true&replicaSet=Classroom-shard-0&authSource=admin&retryWrites=true';
-Mongoose.connect(uri, {useNewUrlParser: true})
+Mongoose.connect(uri || process.env.MONGOLAB_URI, {useNewUrlParser: true})
 .then(() => console.log('connecting to database successful'))
 .catch(err => console.error('could not connect to mongo DB'))
 
